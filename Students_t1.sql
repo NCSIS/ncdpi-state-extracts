9478372a0a3d79f8af43500c5eb1b1cb5c130ce3
jmairs <john@mairs.me>
2024-02-13 12:41:55 -0500

    Use inner joins instead of whatever I was doing

--- Students_t1.sql
+++ Students_t1.sql
@@ -14,11 +14,10 @@ c.[email] as EMAIL,
 NULL as TEACHER_STAFF_ID,
 c.[email] as LEA_ID /*alias ID*/
 
-FROM
-[Training].[dbo].[student] s,
-[Training].[dbo].[school] sch,
-[Training].[dbo].[district] d,
-[Training].[dbo].[Contact] c
+FROM [Training].[dbo].[student] s
+INNER JOIN [Training].[dbo].[school] sch ON sch.[schoolID] = s.[schoolID]
+INNER JOIN [Training].[dbo].[district] d ON d.[districtID] = s.[districtID]
+INNER JOIN [Training].[dbo].[Contact] c ON c.[personID] = s.[personID]
 
 WHERE
 /* Make sure it's an active student who isn't exited in the active calendar */
@@ -26,7 +25,3 @@ s.stateID is not null
 AND s.activeYear=1
 AND s.endStatus is null
 AND s.enrollmentStateExclude=0
-/* Table links */
-AND sch.[schoolID] = s.[schoolID]
-AND d.[districtID] = s.[districtID]
-AND s.[personID] = c.[personID]
