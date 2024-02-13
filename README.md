d469c1fe7f51ab88c5f43a5892066df25d06768b
jmairs <john@mairs.me>
2024-02-13 11:16:22 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#Identity and Access Management SQL Scripts for Infinite Campus
+##Students_t1.sql
+This is an initial draft of a script to pull student data from IC:State Edition in the same format as the existing student source data file.
---
+++ Students_t1.sql
@@ -0,0 +1,16 @@
+SELECT
+[stateID] as PUPIL_NUMBER,
+[lastName] as LAST_NAME,
+[firstName] as FIRST_NAME,
+[middleName] as MIDDLE_NAME,
+[suffix] as NAME_SUFFIX,
+CAST([birthDate] as DATE) as BIRTH_DATE,
+[stateGrade] as GRADE,
+[districtID] as LEA_CODE,
+NULL as LEA_DESC,
+[schoolID] as SCHOOL_CODE,
+[calendarName] as SCHOOL_DESC,
+NULL as EMAIL,
+NULL as TEACHER_STAFF_ID,
+NULL as LEA_ID
+FROM [Training].[dbo].[student] WHERE stateID is not null AND activeYear=1 AND endStatus is null AND enrollmentStateExclude=0
