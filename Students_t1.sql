731a93be37ec2042fc68f11641a6aade81651eb3
jmairs <john@mairs.me>
2024-04-19 15:51:36 -0400

    remove STRING_AGG comments, update table refs

--- Students_t1.sql
+++ Students_t1.sql
@@ -11,17 +11,15 @@ SELECT
     sch.[number] as SCHOOL_CODE,
     sch.[name] as SCHOOL_DESC,
     c.[email] as EMAIL,
-    /* Available in MSSQL 2017 as an alternative... */
-    --STRING_AGG(p.[staffStateID],'::') as TEACHER_STAFF_ID,
-    TEACHER_STAFF_ID = STUFF(
+    STUFF(
         (
             SELECT DISTINCT
                 '::' + p.[staffStateID]
             FROM
-                [ncse_staging].[dbo].[student] s2
-                JOIN [ncse_staging].[dbo].[Roster] r ON r.[personID]=s2.[personID]
-                JOIN [ncse_staging].[dbo].[Section] sec ON sec.[sectionID]=r.[sectionID]
-                JOIN [ncse_staging].[dbo].[Person] p ON sec.[teacherPersonID] = p.[personID]
+                [student] s2
+                JOIN [Roster] r ON r.[personID]=s2.[personID]
+                JOIN [Section] sec ON sec.[sectionID]=r.[sectionID]
+                JOIN [Person] p ON sec.[teacherPersonID] = p.[personID]
             WHERE
                 s2.[stateID] = s.[stateID]
                 AND (
@@ -32,32 +30,21 @@ SELECT
                 ) /* non-ended roster enrollments only */
             FOR XML PATH ('')
         ), 1, 2, ''
-    ),
+    ) as TEACHER_STAFF_ID,
     c.[email] as LOCAL_ID, /*alias ID*/
     CONVERT(VARCHAR(8), s.[modifiedDate], 112) as MOD_DATE
 
 FROM
-    [ncse_staging].[dbo].[student] s /*student view*/
-    JOIN [ncse_staging].[dbo].[school] sch ON sch.[schoolID] = s.[schoolID] /*to get school num and name*/
-    JOIN [ncse_staging].[dbo].[district] d ON d.[districtID] = s.[districtID] /*to get district num and name*/
-    JOIN [ncse_staging].[dbo].[Contact] c ON c.[personID] = s.[personID] /*to get student email*/
-    /* These lines would be for the STRING_AGG function... */
-    --JOIN [ncse_staging].[dbo].[Roster] r ON r.[personID]=s.[personID] /* to get rosters */
-    --JOIN [ncse_staging].[dbo].[Section] sec ON sec.[sectionID]=r.[sectionID] /* to get sections */
-    --JOIN [ncse_staging].[dbo].[Person] p ON sec.[teacherPersonID] = p.[personID] /* to get teacher UIDs */
+    [student] s /*student view*/
+    JOIN [school] sch ON sch.[schoolID] = s.[schoolID] /*to get school num and name*/
+    JOIN [district] d ON d.[districtID] = s.[districtID] /*to get district num and name*/
+    JOIN [Contact] c ON c.[personID] = s.[personID] /*to get student email*/
 
 WHERE
     s.stateID is not null /* QA: UID populated */
     AND s.activeYear=1 /* QA: Current year */
     AND s.endStatus is null /* QA: Not exited */
     AND s.enrollmentStateExclude=0 /* QA: State included */
-    /* These lines would be for the STRING_AGG function... */
-    --AND (
-    --    CAST(r.startDate AS DATE) <= CAST(CURRENT_TIMESTAMP AS DATE)
-    --    ) /* started roster enrollments only */
-    --AND (
-    --    CAST(r.endDate AS DATE) >= CAST(CURRENT_TIMESTAMP AS DATE)
-    --    ) /* non-ended roster enrollments only */
 
 GROUP BY
     s.[stateID],
