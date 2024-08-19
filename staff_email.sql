2007193b76ef1858ba2855873e12de9dc0c0ffe0
jmairs <john@mairs.me>
2024-08-19 16:33:32 -0400

    Filter stale staff emails from prior PSUs

--- staff_email.sql
+++ staff_email.sql
@@ -2,7 +2,8 @@ SELECT DISTINCT
     sm.[staffStateID] as STAFF_UID,
     sm.[schoolNumber] as SCHOOL_CODE,
     sc.[email] as EMAIL,
-    sc.[email] as ALIAS_ID
+    sc.[email] as ALIAS_ID,
+    sm.[personID] as PERSON_ID
 
 FROM
     [staffMember] sm
@@ -13,3 +14,4 @@ WHERE
     AND sc.[email] IS NOT NULL --email populated
     AND sm.[startDate] <= getdate() --start date is today or prior
     AND (sm.[endDate] IS NULL OR sm.[endDate] >= getdate()) --end date is null or future
+    AND sc.districtID = (SELECT [school].[districtID] from [school] where [school].[schoolID] = sm.[schoolID]) --only give us the contacts from the active PSU
