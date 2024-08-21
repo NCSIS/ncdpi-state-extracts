ceaee4dff1aabb92f368dc54a7481c390d0b1ea0
jmairs <john@mairs.me>
2024-08-20 22:00:09 -0400

    Adjust logical order of some filters

--- staff_email.sql
+++ staff_email.sql
@@ -6,12 +6,14 @@ SELECT DISTINCT
 
 FROM
     [staffMember] sm
-    LEFT OUTER JOIN [StudentContact] sc ON sc.[personID] = sm.[personID] AND sc.[relationship] = 'Self'
+    LEFT OUTER JOIN [StudentContact] sc ON
+        sc.[personID] = sm.[personID]
+        AND sc.[relationship] = 'Self' --only get this person's own contact
+        AND sc.[districtID] = (select [school].[districtID] from [school] where [school].[schoolID] = sm.[schoolID]) --only get contacts from the active PSU
+        AND sc.[contactID] = (select max([contactID]) from [StudentContact] where [personID]=sm.[personID]) --only get the max contact ID
 
 WHERE
     LEN(sm.[staffStateID]) = 10 --UID length is 10
     AND sc.[email] IS NOT NULL --email populated
     AND sm.[startDate] <= getdate() --start date is today or prior
     AND (sm.[endDate] IS NULL OR sm.[endDate] >= getdate()) --end date is null or future
-    AND sc.[districtID] = (select [school].[districtID] from [school] where [school].[schoolID] = sm.[schoolID]) --only get contacts from the active PSU
-    AND sc.[contactID] = (select max([contactID]) from [StudentContact] where [personID]=sm.[personID]) --only get the max contact ID
