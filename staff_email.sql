a3df9f3ef743520faf88cf26a0ca07a1c9e0809c
jmairs <john@mairs.me>
2024-10-14 12:14:31 -0400

    Pull both StudentContact and Contact

--- staff_email.sql
+++ staff_email.sql
@@ -4,8 +4,14 @@ SELECT DISTINCT
         WHEN RIGHT(sm.[schoolNumber],3)='810' THEN LEFT(sm.[schoolNumber],3) + '000'
         ELSE sm.[schoolNumber]
     END as SCHOOL_CODE,
-    sc.[email] as EMAIL,
-    sc.[email] as ALIAS_ID
+    CASE
+        WHEN sc.[email] IS NOT NULL THEN sc.[email]
+        ELSE c.[email]
+    END as EMAIL,
+    CASE
+        WHEN sc.[email] IS NOT NULL THEN sc.[email]
+        ELSE c.[email]
+    END as ALIAS_ID
 
 FROM
     [staffMember] sm
@@ -13,10 +19,11 @@ FROM
         sc.[personID] = sm.[personID]
         AND sc.[relationship] = 'Self' --only get this person's own contact
         AND sc.[districtID] = (select [school].[districtID] from [school] where [school].[schoolID] = sm.[schoolID]) --only get contacts from the active PSU
-        AND sc.[contactID] = (select max([contactID]) from [StudentContact] where [personID]=sm.[personID]) --only get the max contact ID
+        AND sc.[contactID] = (select max([contactID]) from [StudentContact] where [personID]=sm.[personID] and [relationship]='Self') --only get the max contact ID
+    LEFT OUTER JOIN [Contact] c ON c.[personID] = sm.[personID]
 
 WHERE
     LEN(sm.[staffStateID]) = 10 --UID length is 10
-    AND sc.[email] IS NOT NULL --email populated
+    AND (sc.[email] IS NOT NULL OR c.[email] IS NOT NULL) --email populated
     AND sm.[startDate] <= getdate() --start date is today or prior
     AND (sm.[endDate] IS NULL OR sm.[endDate] >= getdate()) --end date is null or future
