ee1cecac975818d1e1eb3f2be263b1c96f928017
jmairs <john@mairs.me>
2025-01-28 18:04:03 -0500

    tweaks

--- tools/IC Instance URLs/Get301NewLocations.sh
+++ tools/IC Instance URLs/Get301NewLocations.sh
@@ -1,6 +1,7 @@
 ## Extracts final 301 location for a given list of URLs.
 ## Written by John Mairs, January 2025
 ## Intended to get the full URLs for NCSIS Infinite Campus instances.
+## Used with this Google Sheet: https://docs.google.com/spreadsheets/d/1lc-rdZyOo2hoBgHNHyjdTFCVSv79oRucNmzMRgCReZg/
 
 ## Input is 'urls.txt' and should have one URL per line.
 ## Output is '301NewLocations.txt'
