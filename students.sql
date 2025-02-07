fad3feb98b8494c11de470f6d6c303c5e9e9cfc1
jmairs <john@mairs.me>
2025-02-06 19:53:55 -0500

    Dare requested student alias ID opt-out

--- students.sql
+++ students.sql
@@ -45,7 +45,10 @@ SELECT
             FOR XML PATH ('')
         ), 1, 2, ''
     ) as TEACHER_STAFF_ID,
-    c.[email] as ALIAS_ID,
+    CASE d.[number]
+        WHEN '280' THEN null -- Dare requested an opt-out via email.
+        ELSE c.[email]
+    END as ALIAS_ID,
     FORMAT(s.[modifiedDate],'MM/dd/yyyy') as MOD_DATE
 
 FROM
