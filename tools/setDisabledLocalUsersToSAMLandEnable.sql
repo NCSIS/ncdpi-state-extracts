3a030362b4df2fda3ac1dd82411a3a3180e9406f
John Mairs <john.mairs@dpi.nc.gov>
2025-08-08 09:14:33 -0400

    addtl. validation

--- tools/setDisabledLocalUsersToSAMLandEnable.sql
+++ tools/setDisabledLocalUsersToSAMLandEnable.sql
@@ -1,10 +1,9 @@
 /*
-This script finds users who were migrated into IC via conversion + whose accounts are set to local and disabled, sets those accounts to use SAML: NCEdCloud, then re-enables them.
-This was necessary after NCDPI ran a script to disable all non-SSO users statewide.
+This script finds users who were disabled due to having local accounts + where the username is a UID, sets those accounts to use SAML: NCEdCloud, then re-enables them.
 John Mairs, 8/7/2025
 */
 
 with UsersToFix as(
-    select userID from UserAccount where isSAMLAccount=0 and legacyKey is not null and userID>1
+    select * from UserAccount where isSAMLAccount=0 and legacyKey='8072025' and userID>1 and LEN(username)=10 and ISNUMERIC(username)=1
 )
-update UserAccount set disable=0,lock=0,isSAMLAccount=1,samlConfigurationID=1 where userID in (select userID from UsersToFix)
+update UserAccount set disable=0,lock=0,isSAMLAccount=1,samlConfigurationID=1,legacyKey=null where userID in (select userID from UsersToFix)
