79d8f1c7d4cd9a87933b7fc0132d85e2bbcc699e
John Mairs <john.mairs@dpi.nc.gov>
2026-01-27 17:19:13 -0500

    More efficient logic + more error handling

--- IAM/staff_sn_roles.sql
+++ IAM/staff_sn_roles.sql
@@ -1,268 +1,69 @@
-SELECT
-    STAFF_UID,
-    SCHOOLNET_ROLE
-from (
-    SELECT DISTINCT
+SET NOCOUNT ON;
+
+IF OBJECT_ID('tempdb..#IAMStaffSNRoles') IS NOT NULL DROP TABLE #IAMStaffSNRoles;
+
+DECLARE @asof datetime2 = SYSDATETIME();
+DECLARE @asofEnd datetime2 = dateadd(day,-1,@asof);
+
+CREATE TABLE #IAMStaffSNRoles(
+    STAFF_UID   varchar(10) NOT NULL,
+    SCHOOL_CODE varchar(6)  NOT NULL,
+    SCHOOLNET_ROLE  varchar(15)    NOT NULL,
+    SCHOOLNET_ADD_ROLE nvarchar(max)    NULL
+);
+
+INSERT INTO #IAMStaffSNRoles
+SELECT DISTINCT
         sm.[staffStateID] as STAFF_UID,
         CASE
             WHEN RIGHT(se.[SchoolNumber],3) = '810' THEN LEFT(se.[SchoolNumber],3)
+            /*WHEN RIGHT(se.[SchoolNumber],3) = '810' THEN
+                CASE
+                    WHEN TRY_CAST(LEFT(se.[SchoolNumber],3) as int) IS NOT NULL THEN CAST(LEFT(se.[SchoolNumber],3)*1 as varchar)
+                    ELSE LEFT(se.[SchoolNumber],3)
+                END --if District level needs leading 0 stripped? */
             WHEN TRY_CAST(se.[SchoolNumber] as int) IS NOT NULL THEN CAST(se.[SchoolNumber]*1 as varchar)
             ELSE se.[SchoolNumber]
-        END + ':Leadership' as SCHOOLNET_ROLE,
-        01 as ROLE_NUM
+        END as SCHOOL_CODE,
+        CASE se.[schoolnetRole]
+            WHEN 1 THEN 'Teacher'
+            WHEN 2 THEN 'Leadership'
+            WHEN 3 THEN 'Staff'
+        END as SCHOOLNET_ROLE,
+        LTRIM(RTRIM(value)) as SCHOOLNET_ADD_ROLE
     FROM [v_SchoolEmployment] se
         LEFT OUTER JOIN [staffMember] sm ON sm.[personID]=se.[personID]
+        OUTER APPLY STRING_SPLIT(se.[schoolnetAddRoles], ',')
     WHERE
         len(sm.[staffStateID])=10 --UID is 10 characters in length.
-        AND se.[assignmentStartDate] <= getdate() --start date is today or prior
-        AND (se.[assignmentEndDate] IS NULL OR se.[assignmentEndDate] >= getdate()) --end date is null or future
-        AND se.[schoolnetRole]=2
+        AND LEFT(sm.[staffStateID],1) not in ('0','a','c','d','l')
+        AND sm.[staffStateID]<>'9999999999'
+        AND se.[assignmentStartDate] <= @asof --start date is today or prior
+        AND (se.[assignmentEndDate] IS NULL OR se.[assignmentEndDate] >= @asofEnd) --end date is null or future
+        AND (se.[schoolnetRole] between 1 and 3)
+        AND se.[SchoolNumber] IS NOT NULL;
 
-    UNION ALL
-    -- now Teacher roles...
-    SELECT DISTINCT
-        sm.[staffStateID] as STAFF_UID,
-        CASE
-            WHEN RIGHT(se.[SchoolNumber],3) = '810' THEN LEFT(se.[SchoolNumber],3)
-            WHEN TRY_CAST(se.[SchoolNumber] as int) IS NOT NULL THEN CAST(se.[SchoolNumber]*1 as varchar)
-            ELSE se.[SchoolNumber]
-        END + ':Teacher' as SCHOOLNET_ROLE,
-        02 as ROLE_NUM
-    FROM [v_SchoolEmployment] se
-        LEFT OUTER JOIN [staffMember] sm ON sm.[personID]=se.[personID]
-    WHERE
-        len(sm.[staffStateID])=10 --UID is 10 characters in length.
-        AND se.[assignmentStartDate] <= getdate() --start date is today or prior
-        AND (se.[assignmentEndDate] IS NULL OR se.[assignmentEndDate] >= getdate()) --end date is null or future
-        AND se.[schoolnetRole]=1
+--Pull Primary Roles
+SELECT DISTINCT
+    STAFF_UID as STAFF_UID,
+    SCHOOL_CODE + ':' + SCHOOLNET_ROLE as SCHOOLNET_ROLE
+FROM #IAMStaffSNRoles
 
-    UNION ALL
+--Pull Test Item Admin roles for all
+UNION ALL
+SELECT DISTINCT
+    STAFF_UID as STAFF_UID,
+    SCHOOL_CODE + ':' + SCHOOLNET_ADD_ROLE as SCHOOLNET_ROLE
+FROM #IAMStaffSNRoles
+WHERE SCHOOLNET_ADD_ROLE='Test Item Administrator'
 
-    -- now Staff roles...
-    SELECT DISTINCT
-        sm.[staffStateID] as STAFF_UID,
-        CASE
-            WHEN RIGHT(se.[SchoolNumber],3) = '810' THEN LEFT(se.[SchoolNumber],3)
-            WHEN TRY_CAST(se.[SchoolNumber] as int) IS NOT NULL THEN CAST(se.[SchoolNumber]*1 as varchar)
-            ELSE se.[SchoolNumber]
-        END + ':Staff' as SCHOOLNET_ROLE,
-        03 as ROLE_NUM
-    FROM [v_SchoolEmployment] se
-        LEFT OUTER JOIN [staffMember] sm ON sm.[personID]=se.[personID]
-    WHERE
-        len(sm.[staffStateID])=10 --UID is 10 characters in length.
-        AND se.[assignmentStartDate] <= getdate() --start date is today or prior
-        AND (se.[assignmentEndDate] IS NULL OR se.[assignmentEndDate] >= getdate()) --end date is null or future
-        AND se.[schoolnetRole]=3
+--Pull other additional roles for teachers/staff only
+UNION ALL
+SELECT DISTINCT
+    STAFF_UID as STAFF_UID,
+    SCHOOL_CODE + ':' + SCHOOLNET_ADD_ROLE as SCHOOLNET_ROLE
+FROM #IAMStaffSNRoles
+WHERE SCHOOLNET_ADD_ROLE <> 'Test Item Administrator' AND SCHOOLNET_ROLE <> 'Leadership'
+ORDER BY STAFF_UID asc,SCHOOLNET_ROLE desc;
 
-    UNION ALL
-
-    -- now Access to Teacher and Section Level Data roles...
-    SELECT DISTINCT
-        sm.[staffStateID] as STAFF_UID,
-        CASE
-            WHEN RIGHT(se.[SchoolNumber],3) = '810' THEN LEFT(se.[SchoolNumber],3)
-            WHEN TRY_CAST(se.[SchoolNumber] as int) IS NOT NULL THEN CAST(se.[SchoolNumber]*1 as varchar)
-            ELSE se.[SchoolNumber]
-        END + ':Access to Teacher and Section Level Data' as SCHOOLNET_ROLE,
-        11 as ROLE_NUM
-    FROM [v_SchoolEmployment] se
-        LEFT OUTER JOIN [staffMember] sm ON sm.[personID]=se.[personID]
-    WHERE
-        len(sm.[staffStateID])=10 --UID is 10 characters in length.
-        AND se.[assignmentStartDate] <= getdate() --start date is today or prior
-        AND (se.[assignmentEndDate] IS NULL OR se.[assignmentEndDate] >= getdate()) --end date is null or future
-        AND se.[schoolnetRole] is not null
-        AND se.[schoolnetRole] != 2
-        AND se.[schoolnetAddRoles] like '%Access to Teacher and Section Level Data%'
-
-    UNION ALL
-
-    -- now Access to Aggregate Level Data roles...
-    SELECT DISTINCT
-        sm.[staffStateID] as STAFF_UID,
-        CASE
-            WHEN RIGHT(se.[SchoolNumber],3) = '810' THEN LEFT(se.[SchoolNumber],3)
-            WHEN TRY_CAST(se.[SchoolNumber] as int) IS NOT NULL THEN CAST(se.[SchoolNumber]*1 as varchar)
-            ELSE se.[SchoolNumber]
-        END + ':Access to Aggregate Level Data' as SCHOOLNET_ROLE,
-        12 as ROLE_NUM
-    FROM [v_SchoolEmployment] se
-        LEFT OUTER JOIN [staffMember] sm ON sm.[personID]=se.[personID]
-    WHERE
-        len(sm.[staffStateID])=10 --UID is 10 characters in length.
-        AND se.[assignmentStartDate] <= getdate() --start date is today or prior
-        AND (se.[assignmentEndDate] IS NULL OR se.[assignmentEndDate] >= getdate()) --end date is null or future
-        AND se.[schoolnetRole] is not null
-        AND se.[schoolnetRole] != 2
-        AND se.[schoolnetAddRoles] like '%Access to Aggregate Level Data%'
-
-    UNION ALL
-
-    -- now Access to Assessment Management roles...
-    SELECT DISTINCT
-        sm.[staffStateID] as STAFF_UID,
-        CASE
-            WHEN RIGHT(se.[SchoolNumber],3) = '810' THEN LEFT(se.[SchoolNumber],3)
-            WHEN TRY_CAST(se.[SchoolNumber] as int) IS NOT NULL THEN CAST(se.[SchoolNumber]*1 as varchar)
-            ELSE se.[SchoolNumber]
-        END + ':Access to Assessment Management' as SCHOOLNET_ROLE,
-        13 as ROLE_NUM
-    FROM [v_SchoolEmployment] se
-        LEFT OUTER JOIN [staffMember] sm ON sm.[personID]=se.[personID]
-    WHERE
-        len(sm.[staffStateID])=10 --UID is 10 characters in length.
-        AND se.[assignmentStartDate] <= getdate() --start date is today or prior
-        AND (se.[assignmentEndDate] IS NULL OR se.[assignmentEndDate] >= getdate()) --end date is null or future
-        AND se.[schoolnetRole] is not null
-        AND se.[schoolnetRole] != 2
-        AND se.[schoolnetAddRoles] like '%Access to Assessment Management%'
-
-    UNION ALL
-
-    -- now Access to Curriculum Management roles...
-    SELECT DISTINCT
-        sm.[staffStateID] as STAFF_UID,
-        CASE
-            WHEN RIGHT(se.[SchoolNumber],3) = '810' THEN LEFT(se.[SchoolNumber],3)
-            WHEN TRY_CAST(se.[SchoolNumber] as int) IS NOT NULL THEN CAST(se.[SchoolNumber]*1 as varchar)
-            ELSE se.[SchoolNumber]
-        END + ':Access to Curriculum Management' as SCHOOLNET_ROLE,
-        14 as ROLE_NUM
-    FROM [v_SchoolEmployment] se
-        LEFT OUTER JOIN [staffMember] sm ON sm.[personID]=se.[personID]
-    WHERE
-        len(sm.[staffStateID])=10 --UID is 10 characters in length.
-        AND se.[assignmentStartDate] <= getdate() --start date is today or prior
-        AND (se.[assignmentEndDate] IS NULL OR se.[assignmentEndDate] >= getdate()) --end date is null or future
-        AND se.[schoolnetRole] is not null
-        AND se.[schoolnetRole] != 2
-        AND se.[schoolnetAddRoles] like '%Access to Curriculum Management%'
-
-    UNION ALL
-
-    -- now Access to Teacher Lesson Planner roles...
-    SELECT DISTINCT
-        sm.[staffStateID] as STAFF_UID,
-        CASE
-            WHEN RIGHT(se.[SchoolNumber],3) = '810' THEN LEFT(se.[SchoolNumber],3)
-            WHEN TRY_CAST(se.[SchoolNumber] as int) IS NOT NULL THEN CAST(se.[SchoolNumber]*1 as varchar)
-            ELSE se.[SchoolNumber]
-        END + ':Access to Teacher Lesson Planner' as SCHOOLNET_ROLE,
-        15 as ROLE_NUM
-    FROM [v_SchoolEmployment] se
-        LEFT OUTER JOIN [staffMember] sm ON sm.[personID]=se.[personID]
-    WHERE
-        len(sm.[staffStateID])=10 --UID is 10 characters in length.
-        AND se.[assignmentStartDate] <= getdate() --start date is today or prior
-        AND (se.[assignmentEndDate] IS NULL OR se.[assignmentEndDate] >= getdate()) --end date is null or future
-        AND se.[schoolnetRole] is not null
-        AND se.[schoolnetRole] != 2
-        AND se.[schoolnetAddRoles] like '%Access to Teacher Lesson Planner%'
-
-    UNION ALL
-
-    -- now Test Item Administrator roles...
-    SELECT DISTINCT
-        sm.[staffStateID] as STAFF_UID,
-        CASE
-            WHEN RIGHT(se.[SchoolNumber],3) = '810' THEN LEFT(se.[SchoolNumber],3)
-            WHEN TRY_CAST(se.[SchoolNumber] as int) IS NOT NULL THEN CAST(se.[SchoolNumber]*1 as varchar)
-            ELSE se.[SchoolNumber]
-        END + ':Test Item Administrator' as SCHOOLNET_ROLE,
-        16 as ROLE_NUM
-    FROM [v_SchoolEmployment] se
-        LEFT OUTER JOIN [staffMember] sm ON sm.[personID]=se.[personID]
-    WHERE
-        len(sm.[staffStateID])=10 --UID is 10 characters in length.
-        AND se.[assignmentStartDate] <= getdate() --start date is today or prior
-        AND (se.[assignmentEndDate] IS NULL OR se.[assignmentEndDate] >= getdate()) --end date is null or future
-        AND se.[schoolnetRole] is not null
-        AND se.[schoolnetAddRoles] like '%Test Item Administrator%'
-
-    UNION ALL
-
-    -- now Access for Teachers to Share Assessments roles...
-    SELECT DISTINCT
-        sm.[staffStateID] as STAFF_UID,
-        CASE
-            WHEN RIGHT(se.[SchoolNumber],3) = '810' THEN LEFT(se.[SchoolNumber],3)
-            WHEN TRY_CAST(se.[SchoolNumber] as int) IS NOT NULL THEN CAST(se.[SchoolNumber]*1 as varchar)
-            ELSE se.[SchoolNumber]
-        END + ':Access for Teachers to Share Assessments' as SCHOOLNET_ROLE,
-        17 as ROLE_NUM
-    FROM [v_SchoolEmployment] se
-        LEFT OUTER JOIN [staffMember] sm ON sm.[personID]=se.[personID]
-    WHERE
-        len(sm.[staffStateID])=10 --UID is 10 characters in length.
-        AND se.[assignmentStartDate] <= getdate() --start date is today or prior
-        AND (se.[assignmentEndDate] IS NULL OR se.[assignmentEndDate] >= getdate()) --end date is null or future
-        AND se.[schoolnetRole] is not null
-        AND se.[schoolnetRole] != 2
-        AND se.[schoolnetAddRoles] like '%Access for Teachers to Share Assessments%'
-
-    UNION ALL
-
-    -- now Access to Report Manage roles...
-    SELECT DISTINCT
-        sm.[staffStateID] as STAFF_UID,
-        CASE
-            WHEN RIGHT(se.[SchoolNumber],3) = '810' THEN LEFT(se.[SchoolNumber],3)
-            WHEN TRY_CAST(se.[SchoolNumber] as int) IS NOT NULL THEN CAST(se.[SchoolNumber]*1 as varchar)
-            ELSE se.[SchoolNumber]
-        END + ':Access to Report Manager' as SCHOOLNET_ROLE,
-        18 as ROLE_NUM
-    FROM [v_SchoolEmployment] se
-        LEFT OUTER JOIN [staffMember] sm ON sm.[personID]=se.[personID]
-    WHERE
-        len(sm.[staffStateID])=10 --UID is 10 characters in length.
-        AND se.[assignmentStartDate] <= getdate() --start date is today or prior
-        AND (se.[assignmentEndDate] IS NULL OR se.[assignmentEndDate] >= getdate()) --end date is null or future
-        AND se.[schoolnetRole] is not null
-        AND se.[schoolnetRole] != 2
-        AND se.[schoolnetAddRoles] like '%Access to Report Manage%' --this is a typo in IC
-
-    UNION ALL
-
-    -- now Access to Approve Instructional Materials roles...
-    SELECT DISTINCT
-        sm.[staffStateID] as STAFF_UID,
-        CASE
-            WHEN RIGHT(se.[SchoolNumber],3) = '810' THEN LEFT(se.[SchoolNumber],3)
-            WHEN TRY_CAST(se.[SchoolNumber] as int) IS NOT NULL THEN CAST(se.[SchoolNumber]*1 as varchar)
-            ELSE se.[SchoolNumber]
-        END + ':Access to Approve Instructional Materials' as SCHOOLNET_ROLE,
-        19 as ROLE_NUM
-    FROM [v_SchoolEmployment] se
-        LEFT OUTER JOIN [staffMember] sm ON sm.[personID]=se.[personID]
-    WHERE
-        len(sm.[staffStateID])=10 --UID is 10 characters in length.
-        AND se.[assignmentStartDate] <= getdate() --start date is today or prior
-        AND (se.[assignmentEndDate] IS NULL OR se.[assignmentEndDate] >= getdate()) --end date is null or future
-        AND se.[schoolnetRole] is not null
-        AND se.[schoolnetRole] != 2
-        AND se.[schoolnetAddRoles] like '%Access to Approve Instructional Materials%'
-
-    UNION ALL
-
-    -- now Access to Approve Assessment Items roles...
-    SELECT DISTINCT
-        sm.[staffStateID] as STAFF_UID,
-        CASE
-            WHEN RIGHT(se.[SchoolNumber],3) = '810' THEN LEFT(se.[SchoolNumber],3)
-            WHEN TRY_CAST(se.[SchoolNumber] as int) IS NOT NULL THEN CAST(se.[SchoolNumber]*1 as varchar)
-            ELSE se.[SchoolNumber]
-        END + ':Access to Approve Assessment Items' as SCHOOLNET_ROLE,
-        20 as ROLE_NUM
-    FROM [v_SchoolEmployment] se
-        LEFT OUTER JOIN [staffMember] sm ON sm.[personID]=se.[personID]
-    WHERE
-        len(sm.[staffStateID])=10 --UID is 10 characters in length.
-        AND se.[assignmentStartDate] <= getdate() --start date is today or prior
-        AND (se.[assignmentEndDate] IS NULL OR se.[assignmentEndDate] >= getdate()) --end date is null or future
-        AND se.[schoolnetRole] is not null
-        AND se.[schoolnetRole] != 2
-        AND se.[schoolnetAddRoles] like '%Access to Approve Assessment Items%'
-) ssnr
-WHERE STAFF_UID IS NOT NULL AND LEFT(STAFF_UID,1)<>'0'
-ORDER BY STAFF_UID, ROLE_NUM
+IF OBJECT_ID('tempdb..#IAMStaffSNRoles') IS NOT NULL DROP TABLE #IAMStaffSNRoles;
