a41e7ec4a2d57887190b1932f3f685866e067a18
John Mairs <john.mairs@dpi.nc.gov>
2026-05-29 13:35:08 -0400

    Update where logic to match main student file

--- IAM/student_sn_roles.sql
+++ IAM/student_sn_roles.sql
@@ -1,4 +1,12 @@
-SELECT
+/**********************************************
+Script maintained by NCDPI, PSU Technology Systems Section.
+See https://github.com/NCSIS/ncdpi-state-extracts.
+**********************************************/
+
+DECLARE @asof datetime2 = SYSDATETIME();
+DECLARE @asofEnd datetime2 = dateadd(day,-1,@asof);
+
+SELECT DISTINCT
     s.[stateID] as PUPIL_NUMBER,
     CASE
         WHEN TRY_CAST(sch.[number] as int) IS NOT NULL THEN CAST(sch.[number]*1 as varchar)
@@ -7,10 +15,16 @@ SELECT
 FROM
     [student] s --student view
     LEFT OUTER JOIN [school] sch ON sch.[schoolID] = s.[schoolID] --to get school num and name
-WHERE
-    len(s.[stateID]) between 5 and 10 --UID is between 5 and 10 characters in length.
+    OUTER APPLY (select top 1 endDate from Term join TermSchedule on TermSchedule.termScheduleID=Term.termScheduleID where TermSchedule.structureID=s.structureID order by Term.endDate desc) term --find last day of last term for the year
+WHERE 1=1
+    AND len(s.[stateID]) between 5 and 10 --UID is between 5 and 10 characters in length.
     AND s.[enrollmentStateExclude] = 0 --not state excluded
-    AND (s.[endDate] IS NULL OR s.[endDate] >= getdate()) --end date is null or future
-    AND s.[activeYear] = 1 --is an active enrollment
-    AND s.[noShow] = 0 --isn't a no-show
-    AND s.[serviceType] = 'P' --student service type is primary
+    AND (
+        s.[endDate] IS NULL
+        OR s.[endDate] >= @asofEnd
+        OR (s.[endDate] = term.endDate AND s.[activeYear] = 1)
+    ) --end date is null or future or equal to last day of final term within activeYear
+    AND (
+        s.[activeYear] = 1
+        OR (s.startStatus = 'S1' and s.[startDate] <= @asof)
+    ); --is an activeYear or active Summer enrollment
