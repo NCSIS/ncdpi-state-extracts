6f23956a4fe3582e4fb07853a21e29163249b1fb
John Mairs <john.mairs@dpi.nc.gov>
2026-07-09 16:42:29 -0400

    Teacher backfill 2526, beta logic

---
+++ K3TS/Pre-Kindergarten/TeacherBackfill.sql
@@ -0,0 +1,84 @@
+/**********************************************
+Script maintained by NCDPI, PSU Technology Systems Section.
+See https://github.com/NCSIS/ncdpi-state-extracts.
+**********************************************/
+/*select distinct
+	 i.staffStateID as 'sourceUserID'
+	,CASE WHEN s.number = '296' THEN d.number + ISNULL(cal.number,s.number)
+	      WHEN LEN(s.number) = 3 THEN d.number + s.number 
+		  ELSE s.number 
+		  END as 'sourceSiteID'
+	,i.lastName as 'lastName'
+	,i.firstName as 'firstName'
+	,c.email as 'username'
+	,c.email as 'email'
+	,'1' as 'userTypeID' --1 = teacher, 3 = admin
+	,'1' as 'adminTypeID' --is this correct for teachers?
+	,REPLACE(REPLACE(REPLACE(COALESCE(c.workPhone,c.cellPhone,'5555555555'),'(',''),')',''),'-','') as 'phone'
+	,'3' as 'currTypeID'
+from dbo.District d
+join dbo.School s ON s.districtID = d.districtID
+join dbo.SchoolYear sy ON sy.active = 1
+join dbo.Calendar cal ON cal.endYear = sy.endYear and cal.schoolID = s.schoolID
+join dbo.Trial trl ON trl.calendarID = cal.calendarID and trl.active = 1
+join dbo.Course crs ON crs.calendarID = cal.calendarID and crs.stateCode in ('99329P0','99322P0','99359P0') --only PK Courses
+join dbo.Section sec ON sec.trialID = trl.trialID and sec.courseID = crs.courseID
+join dbo.SectionStaffHistory ssh ON ssh.trialID = trl.trialID and ssh.sectionID = sec.sectionID
+join dbo.Individual i ON i.personID = ssh.personID
+join dbo.Contact c ON c.personID = ssh.personID and c.districtID = d.districtID
+where 1=1
+and ssh.staffType = 'P'
+and (ssh.startDate IS NULL OR ssh.startDate <= '06/30/2026')
+and (ssh.endDate IS NULL OR ssh.endDate >= '07/01/2025')
+and ISNUMERIC(s.number) = 1
+--and RIGHT(s.number,3) >= '300'
+and c.email IS NOT NULL
+
+UNION ALL*/
+
+--anyone with principal in the title for admins
+
+select distinct
+	 i.staffStateID as 'sourceUserID'
+	,CASE WHEN s.number = '296' THEN d.number + ISNULL(cal.number,s.number)
+	      WHEN LEN(s.number) = 3 THEN d.number + s.number 
+		  ELSE s.number 
+		  END as 'sourceSiteID'
+	,i.lastName as 'lastName'
+	,i.firstName as 'firstName'
+	,c.email as 'username'
+	,c.email as 'email'
+	,CASE
+		WHEN ea.k3TSAdminRole IS NULL then '1'
+		WHEN ea.k3TSAdminRole IS NOT NULL then '3'
+	END as 'userTypeID' --1 = teacher, 3 = admin
+	,ea.k3TSAdminRole as 'adminTypeID'
+	,REPLACE(REPLACE(REPLACE(COALESCE(c.workPhone,c.cellPhone,'5555555555'),'(',''),')',''),'-','') as 'phone'
+	,'3' as 'currTypeID'
+from dbo.District d
+join dbo.School s ON s.districtID = d.districtID
+join dbo.SchoolYear sy ON sy.endYear='2026'--sy.active = 1
+join dbo.Calendar cal ON cal.endYear = sy.endYear and cal.schoolID = s.schoolID
+join dbo.EmploymentAssignment ea ON ea.schoolID = s.schoolID
+join dbo.Individual i ON i.personID = ea.personID
+join dbo.Contact c ON c.personID = i.personID and c.districtID = d.districtID
+where 1=1
+and ISNUMERIC(d.number) = 1
+and ea.startDate <= '06/30/2026'
+and (ea.endDate IS NULL)
+and (
+	exists(select 1
+			from dbo.Course crs
+			where crs.calendarID = cal.calendarID
+			and crs.stateCode in ('99329P0','99322P0','99359P0') --only PK Courses
+			and crs.active = 1
+			)
+	or exists(
+		select 1 from GradeLevel where stateGrade in ('IT','PR','PK') and GradeLevel.calendarID=cal.calendarID
+	)
+)
+and ISNUMERIC(s.number) = 1
+--and RIGHT(s.number,3) >= '300'
+--and ea.k3TSAdminRole IS NOT NULL
+and c.email IS NOT NULL
+and i.staffStateID IS NOT NULL
