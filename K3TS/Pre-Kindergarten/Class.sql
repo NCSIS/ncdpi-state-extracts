8195f2909882d614bc1e8ec5011e7fea66b8a871
John Mairs <john.mairs@dpi.nc.gov>
2026-07-28 14:42:37 -0400

    Add length filter on Staff UID.

--- K3TS/Pre-Kindergarten/Class.sql
+++ K3TS/Pre-Kindergarten/Class.sql
@@ -41,4 +41,5 @@ and d.number<>'920'
 and ISNUMERIC(d.number) = 1
 and (ssh.startDate IS NULL OR ssh.startDate <= getdate())
 and (ssh.endDate IS NULL OR ssh.endDate >= getdate())
+and LEN(i.staffStateID) = 10
 group by sec.sectionID,i.staffStateID,i.lastName,sec.number,d.number,s.number,cal.number,crs.number
